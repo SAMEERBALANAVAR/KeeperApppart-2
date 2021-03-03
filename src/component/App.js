1f647dc2ade85eea9815241e7018c2b62e5b061e
SameerBunty <50738759+SAMEERBALANAVAR@users.noreply.github.com>
2021-03-03 14:11:57 +0530

    ntng

--- src/component/App.js
+++ src/component/App.js
@@ -4,16 +4,19 @@ import Header from "./Header";
 import Note from "./Note";
 import notes from "../notes";
 
-function createNotes(noteItem) {
-  return <Note title={noteItem.title} content={noteItem.content} />;
-}
-
 function App() {
   return (
     <div>
       <Header />
       {/* <Note title="This is note" content="This is content" /> */}
-      {notes.map(createNotes)}
+
+      {notes.map((noteItem) => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
       <Footer />
     </div>
   );
